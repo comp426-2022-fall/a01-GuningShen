e0f23c84c44c1275885f580a73aa2709b18d4941
GuningShen <gunings@email.unc.edu>
2022-08-30 21:24:37 -0400

    change server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Require http module
-const http = require('http')
+const http = require('http');
 // Require fs module
 const fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
@@ -8,7 +8,7 @@ const args = require('minimist')(process.argv.slice(2));
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = process.env.PORT || 3000
+const port = args.port || 3000;
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
@@ -21,6 +21,13 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
         console.error(err);
         return;
     }
+// Define a const `server` as an arrow function using http.createServer. 
+// Use the documentation for the node.js http module. 
+// The function should have three responses: 
+// 1. status code 200, 
+// 2. set a header with content type `text/html`, and 
+// 3. end with the data that you are reading in from ./public/index.html.
+    
     const server = http.createServer((req, res) =>
 {
     res.statusCode = 200
@@ -35,13 +42,6 @@ server.listen(port, () => {
 })
 
 });
-
-// Define a const `server` as an arrow function using http.createServer. 
-// Use the documentation for the node.js http module. 
-// The function should have three responses: 
-// 1. status code 200, 
-// 2. set a header with content type `text/html`, and 
-// 3. end with the data that you are reading in from ./public/index.html.
 
 
 // That's it! You're all done!
